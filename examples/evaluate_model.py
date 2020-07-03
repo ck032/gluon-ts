6a50b0a525d7ff714270f688868d3c3492f11d5f
chenkai <chenkai15@geely.com>
2020-07-03 20:38:29 +0800

    模型评估

--- examples/evaluate_model.py
+++ examples/evaluate_model.py
@@ -14,6 +14,12 @@
 """
 This example shows how to fit a model and evaluate its predictions.
 """
+
+# 这样可以保证调试源码的时候，用的是git下载的源码，注意要到src目录
+import sys
+sys.path.insert(0,'/home/chenkai/Documents/git_projects/gluon-ts/src')
+
+
 import pprint
 
 from gluonts.dataset.repository.datasets import get_dataset, dataset_recipes
@@ -22,14 +28,24 @@ from gluonts.evaluation.backtest import make_evaluation_predictions
 from gluonts.model.simple_feedforward import SimpleFeedForwardEstimator
 from gluonts.trainer import Trainer
 
-
 if __name__ == "__main__":
-
+    # 可用的数据集名称，recipes-食谱
     print(f"datasets available: {dataset_recipes.keys()}")
 
     # we pick m4_hourly as it only contains a few hundred time series
+    # 这里采用m4_hourly数据集，因为数据在本地了，所以不需要重新下载
+    # 这个数据集的train.target 有700个值,test = 700 + 48 = 748
     dataset = get_dataset("m4_hourly", regenerate=False)
 
+    print('dataset:\n',dataset)
+    print('dataset.metadata:\n',dataset.metadata)
+    print(len(dataset.train), len(dataset.test))  # 414行
+
+    print(next(iter(dataset.train))['target'].shape)
+    print(next(iter(dataset.test))['target'].shape)
+
+    # simple-MLP网络
+    # 迭代了epochs=5次，num_batches_per_epoch=10,每迭代一次，需要size(dataset)/num_batches_per_epoch次iteration
     estimator = SimpleFeedForwardEstimator(
         prediction_length=dataset.metadata.prediction_length,
         freq=dataset.metadata.freq,
@@ -38,12 +54,16 @@ if __name__ == "__main__":
 
     predictor = estimator.train(dataset.train)
 
+    print(predictor.lead_time)
     forecast_it, ts_it = make_evaluation_predictions(
         dataset.test, predictor=predictor, num_samples=100
     )
 
+    # item_metrics可以理解为每个样本的评估
+    # 比如，多次充电的数据，形成多个样本，每个样本用前700个数据点（温度值）来预测后48个数据点（温度值）
     agg_metrics, item_metrics = Evaluator()(
-        ts_it, forecast_it, num_series=len(dataset.test)
+        ts_it, forecast_it, num_series=len(dataset.test) # 因为Evaluator定义了__call__，所以可以这么传参
     )
 
     pprint.pprint(agg_metrics)
+    pprint.pprint(item_metrics)
